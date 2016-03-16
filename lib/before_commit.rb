bd0aede196a5c9839d0efc0133dca1c9a736c89a
Rob Nichols <robnichols.ea@gmail.com>
2016-03-16 14:33:00 +0000

    Initial gem files

---
+++ lib/before_commit.rb
@@ -0,0 +1,5 @@
+require "before_commit/version"
+
+module BeforeCommit
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'before_commit'
+
+require 'minitest/autorun'
