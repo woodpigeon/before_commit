53ed1482f238fae85361ff9ea4a970f4edba8322
Rob Nichols <robnichols.ea@gmail.com>
2016-03-17 14:56:20 +0000

    Adds facility to copy files in directory to the current location

--- lib/before_commit/file_manager.rb
+++ lib/before_commit/file_manager.rb
@@ -1,6 +1,25 @@
 require 'fileutils'
-class FileManager
-  def self.copy(source, target)
-    FileUtils.copy(source, target)
+require 'pathname'
+module BeforeCommit
+  class FileManager
+    def self.copy(source, target)
+      dir = File.dirname(target)
+      FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
+      FileUtils.cp source, target
+    end
+    
+    def self.copy_dir_to_current(dir)
+      current = FileUtils.pwd
+      dir_path = Pathname dir
+      all_files_in_dir = File.join dir_path, '**', '*'
+      
+      Dir.glob(all_files_in_dir).each do |file|
+        next if Dir.exist?(file)
+        path = Pathname file
+        relative_path = path.relative_path_from(dir_path)
+        target = File.expand_path relative_path, current
+        copy file, target
+      end
+    end
   end
 end
--- test/before_commit/file_manager_test.rb
+++ test/before_commit/file_manager_test.rb
@@ -1,14 +1,72 @@
 require 'test_helper'
 
 module BeforeCommit
-  class FileManagerTest < Minitest::Test
+  class FileManagerTest < Minitest::Test    
+    
+    def teardown
+      File.delete(target) if File.exist? target
+      FileUtils.rm_rf current if Dir.exist? current
+    end
+    
     def test_copy
-      file = data_path('small.yml')
-      target = data_path('temp.yml')
       assert !File.exists?(target), "File should not exist at #{target}"
-      FileManager.copy file, target
+      FileManager.copy source, target
       assert File.exists?(target), "File should exist at #{target}"
-      File.delete target
+    end
+    
+    def test_copy_to_new_subfolder
+      @target = data_path('foo/temp.yml')
+      test_copy
+      FileUtils.rm_rf File.dirname(target)
+    end
+    
+    def test_copy_replaces_existing
+      content = 'Foo Bar'      
+      File.write target, content
+      assert_equal content, target_content
+      FileManager.copy source, target
+      refute_equal content, target_content
+      assert_equal source_content, target_content
+    end
+    
+    def test_copy_dir_to_current
+      make_current_working_directory
+      FileManager.copy_dir_to_current dir
+      ['foo', 'subfolder/bar'].each do |file|
+        source = File.expand_path file, dir
+        target = File.expand_path file, current
+        assert File.exist?(target), "File #{target} should exist"
+        assert FileUtils.identical?(source, target), "File #{file} should be in both locations"
+      end
+    end
+    
+    def source
+      @source ||= data_path('source.yml')
+    end
+    
+    def source_content
+      File.read source
+    end
+    
+    def target
+      @target ||= data_path('temp.yml')
+    end
+    
+    def target_content
+      File.read target
+    end
+    
+    def dir
+      @dir = data_path('sample')
+    end
+    
+    def current
+      @current = data_path('temp')
+    end
+    
+    def make_current_working_directory
+      FileUtils.mkdir_p current
+      FileUtils.cd current
     end
   end
 end
