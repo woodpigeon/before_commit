6d4bec2f003c94275f47cb3f0df1d75d6cc9bf6d
Rob Nichols <robnichols.ea@gmail.com>
2016-03-22 15:40:22 +0000

    Refactor of FileManager to remove some of the complication in copy_content_to_current method

--- lib/before_commit/file_manager.rb
+++ lib/before_commit/file_manager.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 require 'pathname'
 module BeforeCommit
-  module FileManager
+  class FileManager
     def self.copy(source, target)
       dir = File.dirname(target)
       FileUtils.mkdir_p(dir) unless Dir.exist?(dir)
@@ -9,35 +9,57 @@ module BeforeCommit
     end
     
     def self.copy_dir_to_current(dir)
-      dir_path = Pathname dir
-      all_files_in_dir = File.join dir_path, '**', '*'
-      count = 0
-      
-      Dir.glob(all_files_in_dir, include_dotfiles).each do |file|
-        path = Pathname file
-        relative_path = path.relative_path_from(dir_path)
-        target = File.expand_path relative_path, current
-        if Dir.exist?(file)
-          unless dir == file || target == current || ends_with_a_dot?(file)
-            FileUtils.rm_rf target
-          end
-        else
-          copy file, target
-          count += 1
-        end
-      end
-      "#{count} files copied to current location"
-    end
-    
-    def self.include_dotfiles
-      File::FNM_DOTMATCH
+      new(dir).copy_content_to_current
     end
     
     def self.current
       @current ||= FileUtils.pwd
     end
     
-    def self.ends_with_a_dot?(file)
+    attr_reader :dir
+    
+    def initialize(dir)
+      @dir = dir
+    end
+    
+    def copy_content_to_current 
+      count = 0    
+      Dir.glob(all_files_in_dir, include_dotfiles).each do |file|
+        path = Pathname file
+        relative_path = path.relative_path_from(dir_path)
+        target = File.expand_path relative_path, current
+        if Dir.exist?(file)
+          unless dir == file || target == current || ends_with_a_dot?(file)
+            # Assumes directory will be listed first and removes it before
+            # following files are copied into location.
+            # Note `copy` will recreate the directory
+            FileUtils.rm_rf target
+          end
+        else
+          self.class.copy file, target
+          count += 1
+        end
+      end
+      "#{count} files copied to current location"
+    end
+    
+    def dir_path
+      @dir_path ||= Pathname dir
+    end
+    
+    def current
+      self.class.current
+    end
+    
+    def all_files_in_dir
+      File.join dir_path, '**', '*'
+    end
+    
+    def include_dotfiles
+      File::FNM_DOTMATCH
+    end
+    
+    def ends_with_a_dot?(file)
       /\.$/ =~ file
     end
   end
